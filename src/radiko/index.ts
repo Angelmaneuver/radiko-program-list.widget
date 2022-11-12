69a09db1b7725df27bfabe4dc99fd224ae182195
Angelmaneuver <f_a-18hornet@hotmail.co.jp>
2022-11-12 12:07:47 +0900

    Revised logic for retrieving program list.

--- src/radiko/index.ts
+++ src/radiko/index.ts
@@ -8,23 +8,40 @@ const parser = new XMLParser({
 function analysis(xml: string) {
 	const result   = {} as Record<string, unknown>;
 	const stations = (parser.parse(xml)).radiko.stations.station as Array<Record<string, unknown>>;
+	const now      = (() => {
+		const datetime = new Date();
+
+		return `${datetime.getHours().toString().padStart(2, '0')}${datetime.getMinutes().toString().padStart(2, '0')}`;
+	})();
 
 	stations.forEach(
 		(station) => {
 			const name       = station.name as string;
 			const programs   = (station.progs as Record<string, unknown>).prog;
 			const programSet = [] as Array<unknown>;
-			const program    = Array.isArray(programs) ? programs[0] : programs;
+			const program    = (() => {
+				if (Array.isArray(programs)) {
+					for (const program of programs) {
+						if (program.ftl <= now && now < program.tol) {
+							return program;
+						}
+					}
+				} else {
+					return programs;
+				}
+			})();
 
-			programSet.push({
-				title: program.title,
-				time:  program.ftl,
-				img:   program.img,
-				info:  program.info,
-				pfm:   program.pfm,
-			});
-
-			result[name] = programSet;
+			if (program) {
+				programSet.push({
+					title: program.title,
+					time:  program.ftl,
+					img:   program.img,
+					info:  program.info,
+					pfm:   program.pfm,
+				});
+	
+				result[name] = programSet;
+			}
 		}
 	);
 
